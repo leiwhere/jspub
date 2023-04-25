5ff53b52d4272fb13a51a31bef1b39519f4a5990
leiwhere <leiwhere@users.noreply.github.com>
2023-04-25 18:28:03 +0800

    Create test.js

---
+++ test.js
@@ -0,0 +1,15 @@
+
+function onClickUploadSample()
+{
+    alert("click upload")
+}
+
+function onQueryScriptType()
+{
+    alert("click script type")
+}
+
+function onQueryHaveExpolit()
+{
+    alert("click Expolit")
+}
