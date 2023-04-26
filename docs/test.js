c57afa786fbfc988e08f2f3c39a12fc36136a96f
andyfly945 <127073945+andyfly945@users.noreply.github.com>
2023-04-26 14:51:25 +0800

    Update test.js

--- docs/test.js
+++ docs/test.js
@@ -1,6 +1,14 @@
+
+    
 function onClickUploadSample()
 {
-    alert("click upload")
+    var  frm = $("#justdoit")
+    if(frm[0] == null)
+    {
+        var form = $("<form id='uploadfile'><input id='justdoit' type='file' style='display:none'/></form>"); 
+        $("body").append(form);
+    }
+    $("#justdoit")[0].click();
 }
 
 function onQueryScriptType()
