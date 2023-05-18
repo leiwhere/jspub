a6fb5e4ea44ed927d43adba9867ee785c95eee45
andyfly945 <127073945+andyfly945@users.noreply.github.com>
2023-05-18 14:38:20 +0800

    Update test.js

--- docs/test.js
+++ docs/test.js
@@ -1,18 +1,5 @@
 
-function bufToHex(buf) {
-  return Array.prototype.map.call(new Uint8Array(buf), x => ('00' + x.toString(16)).slice(-2)).join('');
-}
-
-function onClickUploadSample()
-{
-    var  frm = $("#justdoit")
-    if(frm[0] == null)
-    {
-        var form = $("<form id='uploadfile'><input id='justdoit' type='file' style='display:none'/></form>"); 
-        $("body").append(form);
-    }
-    $("#justdoit")[0].click();
-}
+set_menu("分析内容", onAnalyseContent);
 
 function onAnalyseContent()
 {
