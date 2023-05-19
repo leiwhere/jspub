8863e2eb35a336f068bf6e767fb7f2639f1a5fcc
andyfly945 <127073945+andyfly945@users.noreply.github.com>
2023-05-19 10:49:37 +0800

    Update test.js

--- docs/test.js
+++ docs/test.js
@@ -10,13 +10,27 @@ msg.push("Set menu name is: 分析内容, func name is: onAnalyseContent")
 $sec_playground.set_menu_func("AI 格式分析", onAI_AnalyseFormat);
 msg.push("Set menu name is: 分析内容, func name is: onAnalyseContent")
 
+$sec_playground.set_message_func("Load Plug", msg)
 msg.push("############# begin lodding plugin ##################")
 
 function onAI_Test()
 {
     $sec_playground.set_message_func("Testing set_message_func", "OK")
-    var hex = $sec_playground.get_hex_content_func()
-    $sec_playground.set_message_func("Testing get_hex_content_func", hex[0:10])
+    
+    var hex     = $sec_playground.get_hex_content_func()
+    $sec_playground.set_message_func("Testing get_hex_content_func, get first 4 byte", hex.slice(0, 4))
+    var ascii   = $sec_playground.get_ascii_content_func()
+    $sec_playground.set_message_func("Testing get_ascii_content, get first 4 byte", ascii.slice(0, 4))
+
+    var content = $sec_playground.get_org_file_content_func()
+    $sec_playground.set_message_func("Testing get_org_file_content_func, get first 4 byte", content.slice(0, 4))
+    
+    $sec_playground.set_plugin_content_func('Hello world !!!')
+    $sec_playground.set_message_func("Testing set_plugin_content_func", "please push button PLUGIN, add see Hello world !!!")
+    
+    var plugin_content = $sec_playground.get_plugin_content_func()
+    $sec_playground.set_message_func("Testing get_plugin_content_func", plugin_content)
+    
     alert("plugin test");
 }
 
