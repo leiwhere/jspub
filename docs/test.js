f7e884fb377df66a576ded17d35d6b0418269678
andyfly945 <127073945+andyfly945@users.noreply.github.com>
2023-05-19 10:31:07 +0800

    Update test.js

--- docs/test.js
+++ docs/test.js
@@ -4,7 +4,7 @@ console.log("load plugin test");
 var msg = []
 msg.push("############# begin lodding plugin ##################")
 
-$sec_playground.set_menu_func("AI 格式分析", onAI_AnalyseFormat);
+$sec_playground.set_menu_func("AI 插件测试", onAI_Test);
 msg.push("Set menu name is: 分析内容, func name is: onAnalyseContent")
 
 $sec_playground.set_menu_func("AI 格式分析", onAI_AnalyseFormat);
@@ -12,6 +12,13 @@ msg.push("Set menu name is: 分析内容, func name is: onAnalyseContent")
 
 msg.push("############# begin lodding plugin ##################")
 
+function onAI_Test()
+{
+    $sec_playground.set_message_func("Testing set_message_func", "OK")
+    var hex = $sec_playground.get_hex_content_func()
+    $sec_playground.set_message_func("Testing get_hex_content_func", hex[0:10])
+    alert("plugin test");
+}
 
 function onAI_AnalyseFormat()
 {
