d133daf0e00380a0cc9e7786a3757b36c2cbb5a7
andyfly945 <127073945+andyfly945@users.noreply.github.com>
2023-06-14 13:28:34 +0800

    Update test.js

--- docs/test.js
+++ docs/test.js
@@ -5,31 +5,29 @@ var msg = []
 msg.push("############# begin lodding plugin ##################")
 
 $SPG$.add_menu("AI 插件测试", onAI_Test);
-msg.push("Set menu name is: AI 插件测试, func name is: onAnalyseContent")
+msg.push("Add menu name is: AI 插件测试, func name is: onAnalyseContent")
 
 $SPG$.add_menu("AI 格式分析", onAI_AnalyseFormat);
-msg.push("Set menu name is: AI 格式分析, func name is: onAnalyseContent")
+msg.push("Add menu name is: AI 格式分析, func name is: onAnalyseContent")
 
 $SPG$.write_msg_console("Load Plug", msg)
 msg.push("############# begin lodding plugin ##################")
 
 function onAI_Test()
 {
-    $SPG$.write_msg_console("Testing set_message_func", "OK")
+    $SPG$.write_msg_console("Testing write_msg_console", "OK")
     
-    var hex     = $SPG$.get_hex_content_func()
-    $SPG$.write_msg_console("Testing get_hex_content_func, get first 4 byte", hex.slice(0, 4))
-    var ascii   = $SPG$.get_ascii_content_func()
-    $SPG$.write_msg_console("Testing get_ascii_content, get first 4 byte", ascii.slice(0, 4))
+    var hex     = $SPG$.get_simple_content_HEX()
+    $SPG$.write_msg_console("Testing get_simple_content_HEX, get first 4 byte", hex.slice(0, 4))
 
-    var content = $SPG$.get_org_file_content_func()
-    $SPG$.write_msg_console("Testing get_org_file_content_func, get first 4 byte", content.slice(0, 4))
+    var content = $SPG$.get_simple_content()
+    $SPG$.write_msg_console("Testing get_simple_content, get first 4 byte", content.slice(0, 4))
     
-    $SPG$.set_plugin_content_func('Hello world !!!')
-    $SPG$.write_msg_console("Testing set_plugin_content_func", "please push button PLUGIN, add see Hello world !!!")
+    $SPG$.add_result('Hello world !!!')
+    $SPG$.write_msg_console("Testing add_result", "please push button PLUGIN, add see Hello world !!!")
     
-    var plugin_content = $SPG$.get_plugin_content_func()
-    $SPG$.write_msg_console("Testing get_plugin_content_func", plugin_content)
+    var plugin_content = $SPG$.get_result()
+    $SPG$.write_msg_console("Testing get_result", plugin_content)
     
     alert("plugin test");
 }
