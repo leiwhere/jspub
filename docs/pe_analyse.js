9ba376686a76e9446b1a782849c8f261c1281017
andyfly945 <127073945+andyfly945@users.noreply.github.com>
2023-06-15 16:56:17 +0800

    Update pe_analyse.js

--- docs/pe_analyse.js
+++ docs/pe_analyse.js
@@ -27,12 +27,12 @@ function onAI_Diasm()
   var question = "你是反汇编专家，请帮我回答一个问题，我输入文件的内容是HEX格式，你帮我进行指令反汇编，我输入的内容是：" 
   question = question + content
 
-  var result = window.chatgpt_query(question)
+  const result = await window.chatgpt_query(question)
   
-  if((await result).STATUS == false)
+  if(result.STATUS == false)
   {
     msg.push("INFO: AI 服务识别失败")
-    msg.push((await result).DATA)
+    msg.push(result.DATA)
     $SPG$.write_msg_console("AI 分析文件格式失败", msg)
     $SPG$.add_result("反汇编:", "ERROR: AI 分析失败, 请检查网络是否正常，或者AI服务KEY是否过期")
   }else{
